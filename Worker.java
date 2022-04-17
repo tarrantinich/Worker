eea519228070d80d7d3aad4d45db31c80516b7bc
Сергей Покровский <pokrovsky.s.n@gmail.com>
2022-04-17 14:23:26 +0300

    first commit

---
+++ Worker.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError( "Task 33 completed with an error");
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
